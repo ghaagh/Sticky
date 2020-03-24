2d5e6c2939483733f507ac899e8288722966040c
Iman Esmaili <ghaagh@gmail.com>
2020-03-24 18:47:24 +0430

    changed_home_page_for_cookie_matching

--- Sticky.API.CookieSyncing/Controllers/HomeController.cs
+++ Sticky.API.CookieSyncing/Controllers/HomeController.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Sticky.API.CookieSyncing.Controllers
 {
+    [Route("Home")]
     public class HomeController : Controller
     {
+        [Route("index")]
+        [HttpGet]
         public string Index()
         {
             return "Hello to you too ;). Also see the cookie syncing extension for more details";
